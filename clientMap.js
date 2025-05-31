7ac589630618036cf641fe1f1992372642a8d100
papa-docs <papa-docs1@proton.me>
2025-06-01 00:13:59 +0300

    Update clientMap.js

--- clientMap.js
+++ clientMap.js
@@ -2,6 +2,6 @@
 //  index.html?client=N
 var CLIENT_MAP = {
   "1pp": { folder: "1", file: "values.js" },
-  "2pp": { folder: "2", file: "values.js" },
-  "dsfdf_dsfds_dsfsd": { folder: "dsfdf_dsfds_dsfsd", file: "values.js" }
+  
+  
 };
